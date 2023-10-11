9103223002a48a71a5a5baa1891aee3e82d95a82
ya7ya99 <yahyakhaldy9@gmail.com>
2023-10-11 16:13:31 -0700

    python1

---
+++ 0x04-python-more_data_structures/Python.h
@@ -0,0 +1,10 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+#endif
