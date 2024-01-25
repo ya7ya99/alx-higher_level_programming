7d8e30739aea4bad7023e292873478aaa630587b
ya7ya99 <yahyakhaldy9@gmail.com>
2024-01-25 13:30:50 -0800

    update

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
-
-# Check if the URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# URL provided as the first argument
-url=$1
-
-# Sending a GET request with curl
-# -H option is used to specify the header variable
-# -s option is used for silent mode (to suppress progress meter)
-# -o option is used to output the response body to stdout
-# -w option is used to display only the response body
-# -X option specifies the HTTP method (GET by default)
-# --header option is used to specify the header
-response=$(curl -s -X GET "$url" -H "X-School-User-Id: 98")
-
-# Displaying the body of the response
-echo "$response"
-
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-School-User-Id: 98" "$1"
